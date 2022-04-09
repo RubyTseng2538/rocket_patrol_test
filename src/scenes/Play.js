ef0878b87daee14fd1df9f01c3496bd2f63603c4
Ruby Tseng <ytseng7@ucsc.edu>
2022-04-09 15:05:34 -0700

    more stuff added

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,7 +17,7 @@ class Play extends Phaser.Scene{
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize* 4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5+ borderPadding*2, 'spaceship', 0, 20). setOrigin(0, 0);
-        this.ship03 = new Spaceship(this, game.config, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -29,6 +29,24 @@ class Play extends Phaser.Scene{
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+        if(this.checkCollision(this.p1Rocket, this.ship03)){
+            console.log('kaboom ship 03');
+        }if(this.checkCollision(this.p1Rocket, this.ship02)){
+            console.log('kaboom ship 02');
+        }if(this.checkCollision(this.p1Rocket, this.ship01)){
+            console.log('kaboom ship 01');
+        }
+    }
+
+    checkCollision(rocket, ship){
+        if(rocket.x <ship.x + ship.width &&
+           rocket.x + rocket.width > ship.x &&
+           rocket.y <ship.y + ship.height &&
+           rocket.height + rocket.y > ship.y){
+               return true;
+        }else{
+            return false;
+        }
     }
 }
 
