d7b3cca9a55b41e8448e1768e749456ecdbfeb2a
Ruby Tseng <ytseng7@ucsc.edu>
2022-04-09 13:32:05 -0700

    minor error

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,7 +3,7 @@ class Menu extends Phaser.Scene{
         super("menuScene");
     }
     create(){
-        this.add.test(20,20,"Rocket Patrol Menu");
+        this.add.text(20,20,"Rocket Patrol Menu");
     }
 }
 
